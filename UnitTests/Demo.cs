dafdbfd410a80ced1db06f74fe027de9de8cd4e6
MartynasVCS <martynasvcs@gmail.com>
2023-08-08 19:45:50 +0300

    Add demo test

---
+++ UnitTests/Demo.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests
+{
+    internal class Demo
+    {
+        [Test]
+        public void Testas()
+        {
+            int sk1 = 2;
+            int sk2 = 7;
+            int expectedResult = 9;
+
+            int actualResult = sk1 - sk2;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
