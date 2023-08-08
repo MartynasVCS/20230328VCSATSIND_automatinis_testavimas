60dbee3e27782791e806a3cea7089509932bfa74
MartynasVCS <martynasvcs@gmail.com>
2023-08-08 22:23:30 +0300

    Add demo selenium test

---
+++ SeleniumTests/Demo.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests
+{
+    public class Demo
+    {
+        [Test]
+        public void OpenGoogle()
+        {
+            string expectedYear = "2023";
+            string expectedDay = "8";
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.day.lt/";
+            IWebElement spanYear = driver.FindElement(By.XPath("//*[@title='Metai']"));
+            IWebElement spanDay = driver.FindElement(By.XPath("//*[@title='Data']"));
+            string actualYear = spanYear.Text;
+            string actualDay = spanDay.Text;
+            driver.Quit();
+
+            Assert.AreEqual(expectedYear, actualYear);
+            Assert.AreEqual(expectedDay, actualDay);
+        }
+    }
+}
