41a9f1e63c8b43abd307e7b2ff4d42479dd48e12
MartynasVCS <martynasvcs@gmail.com>
2023-08-16 20:43:53 +0300

    Add setup and teardown methods

--- SeleniumTests/SeleniumEasy.cs
+++ SeleniumTests/SeleniumEasy.cs
@@ -6,53 +6,54 @@ namespace SeleniumTests
 {
     public class SeleniumEasy
     {
-        [Test]
-        public void SingleInputField()
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
         {
             ChromeOptions options = new ChromeOptions();
             options.AddArgument("--ignore-certificate-errors");
-            IWebDriver driver = new ChromeDriver(options);
+            driver = new ChromeDriver(options);
             driver.Url = "https://demo.seleniumeasy.com/basic-first-form-demo.html";
+        }
+
+        [Test]
+        public void SingleInputField()
+        {
+            string expectedMessage = "Labas";
 
             IWebElement inputMessage = driver.FindElement(By.XPath("//*[@id='get-input']//input"));
             IWebElement buttonShowMessage = driver.FindElement(By.XPath("//*[@id='get-input']//button"));
             IWebElement spanMessage = driver.FindElement(By.XPath("//*[@id='display']"));
-
-            string expectedMessage = "Labas";
-
             inputMessage.SendKeys(expectedMessage);
             buttonShowMessage.Click();
             string actualMessage = spanMessage.Text;
 
             Assert.AreEqual(expectedMessage, actualMessage);
-
-            driver.Quit();
         }
 
         [Test]
         public void TwoInputFields()
         {
-            ChromeOptions options = new ChromeOptions();
-            options.AddArgument("--ignore-certificate-errors");
-            IWebDriver driver = new ChromeDriver(options);
-            driver.Url = "https://demo.seleniumeasy.com/basic-first-form-demo.html";
+            string valueA = "7";
+            string valueB = "8";
+            string expectedResult = "15";
 
             IWebElement inputA = driver.FindElement(By.XPath("//*[@id='value1']"));
             IWebElement inputB = driver.FindElement(By.XPath("//*[@id='value2']"));
             IWebElement buttonGetTotal = driver.FindElement(By.XPath("//*[@id='gettotal']//button"));
             IWebElement spanTotal = driver.FindElement(By.XPath("//*[@id='displayvalue']"));
-
-            string valueA = "7";
-            string valueB = "8";
-            string expectedResult = "15";
-
             inputA.SendKeys(valueA);
             inputB.SendKeys(valueB);
             buttonGetTotal.Click();
             string actualResult = spanTotal.Text;
 
             Assert.AreEqual(expectedResult, actualResult);
+        }
 
+        [TearDown]
+        public void TearDown()
+        {
             driver.Quit();
         }
     }
